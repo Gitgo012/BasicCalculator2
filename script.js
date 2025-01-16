c169a0472c9ad7240ff7e56caa7f0969f31acc38
anmol ranjan <anmol50.ran@gmail.com>
2025-01-16 10:10:19 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function clearDisplay() {
+    document.getElementById("display").value = "";
+  }
+  
+  function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+  }
+  
+  function calculateResult() {
+    try {
+      const display = document.getElementById("display");
+      display.value = eval(display.value);
+    } catch (error) {
+      display.value = "Error";
+    }
+  }
+  
